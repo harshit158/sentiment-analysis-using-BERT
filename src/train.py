92f96d0b64a07d9e16250954f76f7f7c2d61ef7d
harshit sharma <harshit158@gmail.com>
2021-02-09 21:51:50 -0500

    Added argument parsers

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import pandas as pd
 import numpy as np
@@ -12,6 +14,7 @@ import dataset
 import engine
 from model import BERTSentimentModel
 
+
 def get_data_loaders(df, mode='train'):
     my_dataset = dataset.BERTDataset(
         reviews=df.review.values,
@@ -20,14 +23,14 @@ def get_data_loaders(df, mode='train'):
 
     data_loader = torch.utils.data.DataLoader(
         my_dataset,
-        batch_size=config.TRAIN_BATCH_SIZE if mode=='train' else config.VALID_BATCH_SIZE,
+        batch_size=config.TRAIN_BATCH_SIZE if mode == 'train' else config.VALID_BATCH_SIZE,
         num_workers=4
     )
     return data_loader
 
 
-def run():
-    df = pd.read_csv(config.TRAINING_FILE).fillna("none")
+def run(args):
+    df = pd.read_csv(config.TRAINING_FILE, nrows=args['samples']).fillna("none")
 
     # converting sentiments from "positive" and "negative" to 1 and 0
     df.sentiment = df.sentiment.apply(
@@ -35,6 +38,7 @@ def run():
     )
 
     # splitting dataset into train and validation
+
     df_train, df_valid = model_selection.train_test_split(
         df,
         test_size=0.2,
@@ -48,7 +52,9 @@ def run():
 
     # preparing dataloaders
     train_data_loader = get_data_loaders(df_train, mode='train')
+    print(f'Train Batches: {len(train_data_loader)}')
     valid_data_loader = get_data_loaders(df_valid, mode='valid')
+    print(f'Valid Batches: {len(valid_data_loader)}')
 
     # instantiating the BERT model
     model = BERTSentimentModel()
@@ -60,32 +66,51 @@ def run():
     param_optimizer = list(model.named_parameters())
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
     optimizer_parameters = [
-        {'params':[p for n, p in param_optimizer if n not in no_decay], 'weight_decay':0.001},
-        {'params':[p for n, p in param_optimizer if n in no_decay], 'weight_decay':0.0}
+        {'params': [p for n, p in param_optimizer if n not in no_decay],
+            'weight_decay':0.001},
+        {'params': [p for n, p in param_optimizer if n in no_decay],
+            'weight_decay':0.0}
     ]
 
-    num_train_steps = int((len(df_train)/config.TRAIN_BATCH_SIZE)*config.EPOCHS) 
+    num_train_steps = int(
+        (len(df_train)/config.TRAIN_BATCH_SIZE)*config.EPOCHS)
     optimizer = AdamW(optimizer_parameters, lr=3e-5)
     scheduler = get_linear_schedule_with_warmup(
-        optimizer, 
-        num_warmup_steps = 0,
+        optimizer,
+        num_warmup_steps=0,
         num_training_steps=num_train_steps
     )
 
     best_accuracy = 0
-    for epoch in range(config.EPOCHS):
+    for epoch in range(config.EPOCHS if not args['epochs'] else args['epochs']):
         engine.train_fn(train_data_loader, model, optimizer, scheduler, device)
         outputs, targets = engine.eval_fn(valid_data_loader, model, device)
-        
+
         # calculating evaluation metrics
         outputs = np.array(outputs) >= 0.5
         accuracy = metrics.accuracy_score(targets, outputs)
         print(f"Accuracy score = {accuracy}")
-        
+
         # saving model
         if accuracy > best_accuracy:
             torch.save(model.state_dict(), config.MODEL_PATH)
             best_accuracy = accuracy
 
+
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--samples',
+                        type=int,
+                        help='Number of samples to take from the original dataframe (before train-valid split')
+
+    parser.add_argument('-e', '--epochs',
+                        type=int,
+                        help='Number of epochs to train for')
+
+    args = parser.parse_args()
+    samples = args.samples
+    epochs = args.epochs
+
+    args = vars(args)
+    run(args)
